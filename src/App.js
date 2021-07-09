e2abfe5c81231977f3fe87b740fae96469636f75
sungmin <shinandmin13@naver.com>
2021-07-09 13:00:43 +0900

    travis Added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React헐1
+          Learn React헐12
         </a>
       </header>
     </div>
